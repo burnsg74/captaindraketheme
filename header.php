df8d6aacd8dc26ce2769afe14cbb4411130a8419
Greg Burns <gburns@mindresearch.org>
2019-03-01 16:58:28 -0800

    Misc Cleanup

--- header.php
+++ header.php
@@ -1,82 +1,63 @@
-<?php
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
-  <title>Captain Drake's Family Aerial Adventures - The Affordable And Fun Way To Learn To Fly!</title>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible"
-        content="IE=edge">
-  <meta name="viewport"
-        content="width=device-width, initial-scale=1">
-  <meta http-equiv='Content-Language'
-        content='en-us'>
-  <meta name='copyright'
-        content='2012 Captain Drake FAA LLC'>
-  <meta name='rating'
-        content='general'>
-  <meta name='author'
-        content='Captain Drake'>
-  <meta name='description'
-        content='Welcome to Captain Drakes Family Aerial Adventures! We are a place where people who have a love of flying can come to explore their passion.'>
-  <meta name='keywords'
-        content='light sport, sport pilot, aviation,learn to fly, flying, flight training, aircraft, oregon, learning to fly, light sport aircraft,Sport Pilot certificate'>
-  <meta http-equiv='Content-Type'
-        content='text/html; charset=windows-1252'>
-  <meta name='city'
-        content='Cave Junction'>
-  <meta name='state'
-        content='Oregon, OR'>
-  <meta name='robots'
-        content='all=index,follow'>
-  <meta name='revisit-after'
-        content='30 Days'>
-  <link rel='shortcut icon'
-        href='/img/cdfaa.ico'>
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-  <!-- Optional theme -->
-  <link rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
-  <link rel="stylesheet"
-        href="<?php echo esc_url(get_template_directory_uri()); ?>/style.css"/>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport"
-        content="width=device-width">
-  <link rel="profile"
-        href="http://gmpg.org/xfn/11">
-  <link rel="pingback"
-        href="<?php bloginfo('pingback_url'); ?>">
+    <title>Captain Drake's Family Aerial Adventures - The Affordable And Fun Way To Learn To Fly!</title>
+    <meta charset="<?php bloginfo('charset'); ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="profile" href="https://gmpg.org/xfn/11"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv='Content-Language' content='en-us'>
+    <meta name='copyright' content='2012 Captain Drake FAA LLC'>
+    <meta name='rating' content='general'>
+    <meta name='author' content='Captain Drake'>
+    <meta name='description'
+          content='Welcome to Captain Drakes Family Aerial Adventures! We are a place where people who have a love of flying can come to explore their passion.'>
+    <meta name='keywords'
+          content='light sport, sport pilot, aviation,learn to fly, flying, flight training, aircraft, oregon, learning to fly, light sport aircraft,Sport Pilot certificate'>
+    <meta http-equiv='Content-Type' content='text/html; charset=windows-1252'>
+    <meta name='city' content='Cave Junction'>
+    <meta name='state' content='Oregon, OR'>
+    <meta name='robots' content='all=index,follow'>
+    <meta name='revisit-after' content='30 Days'>
+    <link rel='shortcut icon' href='/img/cdfaa.ico'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="<?php echo esc_url(get_template_directory_uri()); ?>/style.css"/>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <link rel="pingback"
+          href="<?php bloginfo('pingback_url'); ?>">
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<div class="container"
-     id='MasterTable'>
-  <div style="text-align: center;">
-    <table>
-      <tr>
-        <td colspan='2'
-            style='height: 250px; background: url(<?php echo esc_url(get_template_directory_uri()); ?>/img/cdfaa.jpg) repeat; '>
-          <div id='Contact'>
-            Contact Us: (541) 415-0600 <br>
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <td colspan='2'
-            style='background-color: rgb(182,182,182);'>
-          <div id='MainMenu'>
-            <ul class="nav navbar-nav">
-              <li class="active"><a href='/'>Home</a></li>
-              <li><a href='/learntofly'>Learn To Fly</a></li>
-              <li><a href='/packages'>Packages</a></li>
-              <li><a href='/faq'>Light Sport FAQ</a></li>
-              <li><a href='/contact'>Contact Us</a></li>
-              <li><a href='/lodge'>The Lodge</a></li>
-              <li><a href='/plane'>N51DV</a></li>
-              <li><a href='/links'>Links</a></li>
-            </ul>
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <td style='color: rgb(224,224,224); width: 700px; background-color: rgb(20,20,20); font-size: 14px; text-align: justify; padding-top: 20px; padding-bottom: 10px; padding-left: 50px; padding-right: 50px;'>
+<div id="page" class="site">
+<div class="container" id='MasterTable'>
+    <div style="text-align: center;">
+        <table>
+            <tr>
+                <td colspan='2'
+                    style='height: 250px; background: url(<?php echo esc_url(get_template_directory_uri()); ?>/img/cdfaa.jpg) repeat; '>
+                    <div id='Contact'>
+                        Contact Us: (541) 415-0600 <br>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td colspan='2'
+                    style='background-color: rgb(182,182,182);'>
+                    <div id='MainMenu'>
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href='/'>Home</a></li>
+                            <li><a href='/learntofly'>Learn To Fly</a></li>
+                            <li><a href='/packages'>Packages</a></li>
+                            <li><a href='/faq'>Light Sport FAQ</a></li>
+                            <li><a href='/contact'>Contact Us</a></li>
+                            <li><a href='/lodge'>The Lodge</a></li>
+                            <li><a href='/plane'>N51DV</a></li>
+                            <li><a href='/links'>Links</a></li>
+                        </ul>
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td style='color: rgb(224,224,224); width: 700px; background-color: rgb(20,20,20); font-size: 14px; text-align: justify; padding-top: 20px; padding-bottom: 10px; padding-left: 50px; padding-right: 50px;'>
