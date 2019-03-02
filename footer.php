e5f263bf3c03dc179bcfb2c4d495050267d78837
Gregory D. Burns <greg@burnswebservices.com>
2019-03-02 00:21:35 +0000

    Init

---
+++ footer.php
@@ -0,0 +1,25 @@
+	</div><!-- .site-content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+      Phone: (541) 415-0600 * Email: <a href='mailto:captaindrakesfaa@gmail.com'
+                                        title='Email Captain Drake'>Captaindrakesfaa@gmail.com</a><br>&copy;
+      2012 Captain Drake's Family Aerial Adventures LLC. - <a href='/terms.html'>Terms &
+        Conditions</a> - <a
+          href='/privacy.html'>Privacy Policy</a>
+      <center>Photos of N51DV * 2013 Christina Paul Photography</center>
+		</div><!-- .site-info -->
+	</footer><!-- .site-footer -->
+
+</div><!-- .site -->
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135350598-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-135350598-1');
+</script>
+</body>
+</html>
